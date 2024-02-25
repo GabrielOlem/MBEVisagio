2f789ee5afd88b71573149dc6d724ef4916f52c3
Gabriel Melo <gme@cin.ufpe.br>
2024-02-25 13:46:17 -0300

    Updates

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 from typing import Final
+import ast
 from tabulate import tabulate
+from datetime import datetime, timezone
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import pandas as pd
@@ -26,13 +28,25 @@ async def update_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         update_placar(context.args[0], context.args[1], context.args[2])
         await update.message.reply_text('Placar atualizado com sucesso')
 
-async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def write_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global WKS
     global PLACAR
     global ADMIN
     if update.message.from_user.id == ADMIN:
         WKS.set_dataframe(PLACAR,(1,1))
         await update.message.reply_text('Google sheets atualizado com sucesso')
+
+async def read_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global WKS
+    global PLACAR
+    global ADMIN
+    if update.message.from_user.id == ADMIN:
+        PLACAR = WKS.get_as_df()
+        PLACAR.Integrantes = PLACAR.Integrantes.apply(mySetConv)
+        PLACAR.Meetup = PLACAR.Meetup.map({'TRUE': True, 'FALSE': False})
+        PLACAR.Vibe = PLACAR.Vibe.map({'TRUE': True, 'FALSE': False})
+        PLACAR.Atividade1 = PLACAR.Atividade1.map({'TRUE': True, 'FALSE': False})
+        await update.message.reply_text('PLACAR atualizado com sucesso')
 
 # Handle Responses
 def reset():
@@ -42,7 +56,7 @@ def reset():
     PLACAR['Vibe'] = False
     PLACAR['AtivFisica'] = 0
     PLACAR['AtivRelax'] = 0
-    PLACAR['Atividade1'] = 0
+    PLACAR['Atividade1'] = False
     return 'Tabela Resetada com Sucesso'
     
 def update_placar(nomeDupla, valor, coluna):
@@ -50,23 +64,27 @@ def update_placar(nomeDupla, valor, coluna):
     idx = PLACAR.Dupla.explode().eq(nomeDupla).loc[lambda x: x].index.values[0]
     PLACAR.at[idx, coluna] = valor
 
+def mySetConv(txt):
+    return set() if txt == 'set()' else ast.literal_eval(txt)
+
 def pontuar(idPessoal, pontos, typ):
     global PLACAR
+    global WKS
     idx = PLACAR.Integrantes.explode().eq(idPessoal).loc[lambda x: x].index
     al = PLACAR.loc[idx]
     if al.empty:
         return f'Usuário não registrado em nenhuma dupla'
     al = al.iloc[0]
     if PLACAR.at[idx.values[0], "ValorDiario"] == 6:
-        return f'Pontuação máxima diária já atingida'
+        return f'Parabéns, a pontuação máxima diária de 6 pontos já foi atingida pela dupla "{PLACAR.at[idx.values[0], "Dupla"]}"! Faça mais atividades amanhã para garantir ainda mais pontos.'
     mini = min(6 - PLACAR.at[idx.values[0], "ValorDiario"], pontos)
-
     if typ == 'Meetup':
         if al['Meetup'] == True:
             return f'Atividade Meetup já foi registrada anteriormente para a dupla "{al["Dupla"]}"'
         PLACAR.at[idx.values[0], "Meetup"] = True
         PLACAR.at[idx.values[0], "Placar"] += mini
         PLACAR.at[idx.values[0], "ValorDiario"] += mini
+        WKS.set_dataframe(PLACAR,(1,1))
         return f'Atividade Meetup registrada com sucesso para a dupla "{al["Dupla"]}"'
     elif typ == 'Vibe':   
         if al['Vibe'] == True:
@@ -74,6 +92,7 @@ def pontuar(idPessoal, pontos, typ):
         PLACAR.at[idx.values[0], "Vibe"] = True
         PLACAR.at[idx.values[0], "Placar"] += mini
         PLACAR.at[idx.values[0], "ValorDiario"] += mini
+        WKS.set_dataframe(PLACAR,(1,1))
         return f'Atividade Vibe registrada com sucesso para a dupla "{al["Dupla"]}"'
     elif typ == 'Atividade1':   
         if al['Atividade1'] == True:
@@ -81,6 +100,7 @@ def pontuar(idPessoal, pontos, typ):
         PLACAR.at[idx.values[0], "Atividade1"] = True
         PLACAR.at[idx.values[0], "Placar"] += mini
         PLACAR.at[idx.values[0], "ValorDiario"] += mini
+        WKS.set_dataframe(PLACAR,(1,1))
         return f'Atividade1 registrada com sucesso para a dupla "{al["Dupla"]}"'    
     elif typ == 'AtivFisica':
         if al['AtivFisica'] == sum(PLACAR.at[idx.values[0], "Integrantes"]):
@@ -90,6 +110,7 @@ def pontuar(idPessoal, pontos, typ):
         PLACAR.at[idx.values[0], "AtivFisica"] += idPessoal
         PLACAR.at[idx.values[0], "Placar"] += mini
         PLACAR.at[idx.values[0], "ValorDiario"] += mini
+        WKS.set_dataframe(PLACAR,(1,1))
         return f'A atividade física registrada com sucesso para a dupla "{al["Dupla"]}"'
     elif typ == 'AtivRelax':
         if al['AtivRelax'] == sum(PLACAR.at[idx.values[0], "Integrantes"]):
@@ -99,6 +120,7 @@ def pontuar(idPessoal, pontos, typ):
         PLACAR.at[idx.values[0], "AtivRelax"] += idPessoal
         PLACAR.at[idx.values[0], "Placar"] += mini
         PLACAR.at[idx.values[0], "ValorDiario"] += mini
+        WKS.set_dataframe(PLACAR,(1,1))
         return f'A atividade relax registrada com sucesso para a dupla "{al["Dupla"]}"'
         
         
@@ -109,62 +131,78 @@ def handle_response(text: str, update: Update) -> str:
     idPessoal = update.message.from_user.id
 
     if '/registrar' == processed:
-        return 'Necessário inserção do nome da dupla'
+        return 'Comando não válido. Formato de uso: "/registrar "NOME DA DUPLA"'
     
     if '/registrar' in processed:
         nomedupla = processed.split('/registrar')[1].strip()
         al = PLACAR.loc[PLACAR.Integrantes.explode().eq(idPessoal).loc[lambda x: x].index, "Dupla"]
+        troca = False
         if not al.empty:
-            return f'Usuário já registrado na dupla "{al.iloc[0]}"'
+            if update.message.date > datetime(2024, 3, 2, 3, 0, tzinfo=timezone.utc):
+                return f'Opa, {update.message.from_user.first_name}. Você já está registrado na dupla "{al.iloc[0]}"'
+            else:
+                PLACAR.loc[al.index[0], 'Integrantes'].remove(idPessoal)
+                troca = True
+                if len(PLACAR.loc[al.index[0], 'Integrantes']) == 0:
+                    PLACAR.drop(al.index[0], inplace=True)
         
         if nomedupla not in PLACAR.Dupla.values:
             PLACAR = pd.concat([PLACAR, pd.DataFrame({'Dupla': [nomedupla], 'Integrantes': [{idPessoal}], 'Placar': [0], 'ValorDiario': [0], 'Meetup': [False], 'Vibe': [False], 'AtivFisica': [0], 'AtivRelax': [0], 'Atividade1': [False]})], axis=0, ignore_index=True)
-            return 'Dupla Registrada com sucesso'
+            if troca:
+                return f'Dupla trocada com sucesso, {update.message.from_user.first_name}!'
+            return f'Seja muito bem vindo ao grupo do mês do bem estar, {update.message.from_user.first_name}'
         else:
             row = PLACAR[PLACAR['Dupla'] == nomedupla].iloc[0]
             if len(row.Integrantes) == 2:
-                return f'A dupla "{nomedupla}" já está cheia'
+                return f'A dupla "{nomedupla}" já tem duas pessoas, use outro nome por favor.'
             else:
                 PLACAR.loc[row.name, 'Integrantes'].add(idPessoal)
-                return f'A dupla "{nomedupla}" foi completada com sucesso'
+                return f'Seja muito bem vindo ao grupo do mês do bem estar, {update.message.from_user.first_name}. Sua dupla agora está completa.'
        
-    
+    if update.message.date < datetime(2024, 3, 1, 3, 0, tzinfo=timezone.utc) and processed in ['/meetup', '/ativfisica', '/ativrelax', '/vibe']:
+        return f'A atividade "{processed[1:]}" só será permitida a partir do dia 1 de março'
     if '/atividade1' == processed:
+        if update.message.date > datetime(2024, 3, 3, 3, 0, tzinfo=timezone.utc):
+            return 'O prazo para envio da atividade1 foi finalizado no dia 2 de março'
         if update.message.photo == ():
             return 'Atividade sem imagem não será registrada'
         return pontuar(idPessoal, 4, 'Atividade1')
 
     if '/placar' == processed:
+        stri = "Placar atual:\n\n"
         p = PLACAR[["Dupla", "Placar"]]
         p["Placar"] = p["Placar"].astype('int')
-        m = p.Dupla.astype(str).str.len().max()
-        p.Dupla = p.Dupla.astype(str).str.ljust(m, ' ')
-        p = p.to_string(index = False)
-
-        return f"```{p}```"
-        
+        p.sort_values(inplace=True, by="Placar", ascending=False)
+        # m = p.Dupla.astype(str).str.len().max()
+        # p.Dupla = p.Dupla.astype(str).str.ljust(m, ' ')
+        # p = p.to_string(index = False)
+        for i, r in p.iterrows():
+            print(len(str(r['Placar'])), r['Placar'])
+            con = 15 - len(str(r['Placar']))
+            stri += " "*con + f"{r['Placar']} | {r['Dupla']}\n"
+        return stri
     
     if '/meetup' == processed:
         if update.message.photo == ():
-           return 'Atividade sem imagem não será registrada'
+            return f"{update.message.from_user.first_name}, para registrar atividade é obrigatório o envio de uma imagem junto com o comando."
         return pontuar(idPessoal, 4, 'Meetup')
 
     if '/ativfisica' == processed:
         if update.message.photo == ():
-            return 'Atividade sem imagem não será registrada'
+            return f"{update.message.from_user.first_name}, para registrar atividade é obrigatório o envio de uma imagem junto com o comando."
         return pontuar(idPessoal, 1, 'AtivFisica')
     
     if '/ativrelax' == processed:
         if update.message.photo == ():
-            return 'Atividade sem imagem não será registrada'
+            return f"{update.message.from_user.first_name}, para registrar atividade é obrigatório o envio de uma imagem junto com o comando."
         return pontuar(idPessoal, 1, 'AtivRelax')
     
     if '/vibe' == processed:
         if update.message.photo == ():
-            return 'Atividade sem imagem não será registrada'
+            return f"{update.message.from_user.first_name}, para registrar atividade é obrigatório o envio de uma imagem junto com o comando."
         return pontuar(idPessoal, 4, 'Vibe')
     
-    return 'Comando não reconhecido'
+    return f'{update.message.from_user.first_name}, comando não reconhecido. Digite /start para visualizar os comandos disponíveis'
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
@@ -178,7 +216,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
         print('Bot: ', response)
         if response != '':
-            await update.message.reply_text(response, parse_mode="Markdown")
+            await update.message.reply_text(response)
     
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -190,7 +228,8 @@ if __name__ == '__main__':
 
     app.add_handler(CommandHandler('reset', reset_command))
     app.add_handler(CommandHandler('update', update_command))
-    app.add_handler(CommandHandler('custom', custom_command))
+    app.add_handler(CommandHandler('write', write_command))
+    app.add_handler(CommandHandler('read', read_command))
 
 
     app.add_handler(MessageHandler(filters.ALL, handle_message))
